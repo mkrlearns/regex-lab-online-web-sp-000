e2528b89f698ab14b6aff1b41d6e0eeceb630972
ryan m <contact@ryanmeek.com>
2019-02-17 00:56:08 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,9 +11,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z].*[?.,:;]$/)
+  text.match(/^[A-Z].*[?.,:;]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-  text.match(/\d{10}|\d{3}\S\d{3}\S\d{4}/)
+  phone.match(/\d{10}|\d{3}\S\d{3}\S\d{4}/)
 end
